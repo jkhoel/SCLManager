e9c7bb1cd198d74fad4fdb7d2db3f282f9b9a4ad
jkhoel <git@hoelweb.com>
2023-06-21 19:04:15 +0200

    add check for deadcode to local clippy script

--- cargo-watch-clippy.sh
+++ cargo-watch-clippy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Running this command requires https://crates.io/crates/cargo-watch to be installed
-cargo watch -x 'clippy --fix --allow-dirty'
+cargo watch -x 'clippy --fix --allow-dirty -- -D warnings'
