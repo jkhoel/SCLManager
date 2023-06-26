862479eb77503a71928d7d0c09523cc34adc97a3
jkhoel <git@hoelweb.com>
2023-06-27 00:28:07 +0200

    better transition for listbox

--- components/ui/listbox/index.tsx
+++ components/ui/listbox/index.tsx
@@ -61,9 +61,12 @@ const UIListBox: React.FC<ListboxProps> = ({ items, callback }) => {
           </Listbox.Button>
           <Transition
             as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {items.map((item, itemIdx) => (
