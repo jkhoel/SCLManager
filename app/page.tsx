a713b9771ed749d8400dea416486fd54c86fcc67
Jens Kristian Hoel <jens.hoel@norgesgruppen.no>
2023-06-27 15:37:04 +0200

    styling and app rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import { invoke } from "@tauri-apps/api/tauri";
 import { Airframe, FlyableAirframe, Handlers } from "@/bindings/rust";
-import { Listbox } from "@headlessui/react";
-import UIListBox, { ListboxItem } from "@/components/ui/listbox";
-import loadConfig from "next/dist/server/config";
 
-const fakeItems: ListboxItem[] = [
+import StyledListBox, { StyledListBoxItem } from "@/components/ui/listbox";
+import BrandText from "@/components/ui/branding";
+
+const fakeItems: StyledListBoxItem[] = [
   { id: 1, label: "foo", value: "foo" },
   { id: 2, label: "bar", value: "bar" },
   { id: 3, label: "baz", value: "baz" },
@@ -17,7 +16,7 @@ const fakeItems: ListboxItem[] = [
 
 export default function Home() {
   const [flyableAirframes, setFlyableAirframes] = useState<FlyableAirframe[]>();
-  const [selected, setSelected] = useState<ListboxItem>({
+  const [selected, setSelected] = useState<StyledListBoxItem>({
     id: 1,
     label: "Hello world!",
     value: 1,
@@ -35,21 +34,29 @@ export default function Home() {
 
   console.log(flyableAirframes);
 
-  const handleAirframeSelect = (selectedItem: ListboxItem) => {
+  const handleAirframeSelect = (selectedItem: StyledListBoxItem) => {
     setSelected(selectedItem);
   };
 
   return (
     <main>
-      <div className="relative flex min-h-screen p-4 flex-col justify-start items-center overflow-hidden bg-slate-900 py-6 text-base leading-7 text-gray-600">
-        <div className="grid grid-flow-row auto-rows-max min-w-full bg-white px-4 py-4 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-          <div className="bg-white p-4">
-            <AirframeSelectListBox
-              airframes={flyableAirframes}
-              callback={handleAirframeSelect}
-            />
+      <div className="relative flex min-h-screen flex-col justify-start items-center overflow-hidden bg-slate-900 text-base leading-7 text-black">
+        <div className="grid grid-flow-row auto-rows-max min-w-full mx-2">
+          <div
+            id="header-menu-container"
+            className="p-4 grid grid-flow-col justify-between bg-slate-800"
+          >
+            <div>
+              <AirframeSelectListBox
+                airframes={flyableAirframes}
+                callback={handleAirframeSelect}
+              />
+            </div>
+            <div className="justify-end flex items-center ">
+              <BrandText />
+            </div>
           </div>
-          <div className="mx-auto">{selected.label}</div>
+          <div className="mx-auto text-white">{selected.label}</div>
         </div>
       </div>
     </main>
@@ -58,22 +65,24 @@ export default function Home() {
 
 type AirframeSelectListBox = {
   airframes?: FlyableAirframe[];
-  callback?: (selectedItem: ListboxItem) => void;
+  callback?: (selectedItem: StyledListBoxItem) => void;
 };
 
 const AirframeSelectListBox: React.FC<AirframeSelectListBox> = ({
   airframes,
   callback,
 }) => {
-  const [airframeItems, setAirframeItems] = useState<ListboxItem[]>();
+  const [airframeItems, setAirframeItems] = useState<StyledListBoxItem[]>();
 
   useEffect(() => {
     if (airframes) {
-      const items: ListboxItem[] = airframes.map((airframe, airframeIdx) => ({
-        id: airframe.id ?? airframeIdx,
-        label: airframe.name,
-        value: airframe.id ?? airframeIdx,
-      }));
+      const items: StyledListBoxItem[] = airframes.map(
+        (airframe, airframeIdx) => ({
+          id: airframe.id ?? airframeIdx,
+          label: airframe.name,
+          value: airframe.id ?? airframeIdx,
+        })
+      );
 
       setAirframeItems(items);
     }
@@ -82,7 +91,7 @@ const AirframeSelectListBox: React.FC<AirframeSelectListBox> = ({
   if (!airframeItems) return <></>;
 
   return (
-    <UIListBox
+    <StyledListBox
       items={airframeItems}
       callback={callback ? callback : console.log}
     />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
+};
